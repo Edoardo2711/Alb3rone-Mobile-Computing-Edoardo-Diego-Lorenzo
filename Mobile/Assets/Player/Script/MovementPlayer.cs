368346ef74a08c832724e24dd8ab2b22bbb22fc7
Alberone <vied.03278@gmail.com>
2026-03-06 18:16:43 +0100

    Ristrutturato tutto il codice
    Fatto Funzionare correttamente il player e le animazioni
    Tolta la possibilità di sparare(da riaggiungere dopo)
    tolti script ormai inutili avendo modificato il funzionamento del player

---
+++ Mobile/Assets/Player/Script/MovementPlayer.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementPlayer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float Velocita;
+    private Rigidbody2D rb;
+
+    private Animator animator;
+    private Vector2 moveInput; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = moveInput * Velocita;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
+        animator.SetBool("Camminando", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("Camminando", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
+        moveInput = context.ReadValue<Vector2>();
+
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+
+        }
+}
